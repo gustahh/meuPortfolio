0a798859d43457b58facfad31415b56b638263e3
Gustavo Graciano <gustavocarlos206@gmail.com>
2024-02-05 21:08:47 -0300

    commit changes

--- index.js
+++ index.js
@@ -3,13 +3,21 @@ $(document).ready(function() {
         id = $(this).attr('id');
         console.log(id);
         if (id === 'bemvindo') {
-            $('.nomePag').html('Bem vindo!'); 
+            $('.nomePag').html('Bem vindo!');
+            $(this)find('img').attr('src', 'icons/house.svg'); 
+            $(this).find('img').attr('src', 'icons/msg-bubble-user-fill.svg');
+            $(this).next().find('img').attr('src', 'icons/layers.svg');
+            $(this).next().next().find('img').attr('src', 'icons/at-sign.svg');
         } else if (id === 'sobre') {
-
+            $('.nomePag').html('Sobre mim');
+            $(this).prev().find('img').attr('src', 'icons/house.svg'); 
+            $(this).find('img').attr('src', 'icons/msg-bubble-user-fill.svg');
+            $(this).next().find('img').attr('src', 'icons/layers.svg');
+            $(this).next().next().find('img').attr('src', 'icons/at-sign.svg');     
         } else if (id === 'portfolio') {
-            
+            $('.nomePag').html('Portfólio'); 
         } else if (id === 'contato') {
-
+            $('.nomePag').html('Contato'); 
         }
     });
 });
