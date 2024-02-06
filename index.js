c2240eb59c595e20678e93ed3e99adb0508a76d1
Gustavo Graciano <gustavocarlos206@gmail.com>
2024-02-05 21:14:20 -0300

    commit changes

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ $(document).ready(function() {
         console.log(id);
         if (id === 'bemvindo') {
             $('.nomePag').html('Bem vindo!');
-            $(this)find('img').attr('src', 'icons/house.svg'); 
-            $(this).find('img').attr('src', 'icons/msg-bubble-user-fill.svg');
-            $(this).next().find('img').attr('src', 'icons/layers.svg');
-            $(this).next().next().find('img').attr('src', 'icons/at-sign.svg');
+            $(this).find('img').attr('src', 'icons/house-fill.svg'); 
+            $(this).next().find('img').attr('src', 'icons/msg-bubble-user.svg');
+            $(this).next().next().find('img').attr('src', 'icons/layers.svg');
+            $(this).next().next().next().find('img').attr('src', 'icons/at-sign.svg');
         } else if (id === 'sobre') {
             $('.nomePag').html('Sobre mim');
             $(this).prev().find('img').attr('src', 'icons/house.svg'); 
@@ -15,9 +15,25 @@ $(document).ready(function() {
             $(this).next().find('img').attr('src', 'icons/layers.svg');
             $(this).next().next().find('img').attr('src', 'icons/at-sign.svg');     
         } else if (id === 'portfolio') {
-            $('.nomePag').html('Portfólio'); 
+            $('.nomePag').html('Portfólio');
+            $(this).prev().prev().find('img').attr('src', 'icons/house.svg'); 
+            $(this).prev().find('img').attr('src', 'icons/msg-bubble-user.svg');
+            $(this).find('img').attr('src', 'icons/layers-fill.svg');
+            $(this).next().find('img').attr('src', 'icons/at-sign.svg'); 
         } else if (id === 'contato') {
-            $('.nomePag').html('Contato'); 
+            $('.nomePag').html('Contato');
+            $(this).prev().prev().prev().find('img').attr('src', 'icons/house.svg'); 
+            $(this).prev().prev().find('img').attr('src', 'icons/msg-bubble-user.svg');
+            $(this).prev().find('img').attr('src', 'icons/layers.svg');
+            $(this).find('img').attr('src', 'icons/at-sign-fill.svg'); 
         }
+        /* 
+            !explicando código!
+            .next() seleciona o elemento próximo a div clicada (this, event),
+            cada .next() a mais é um elemento a mais. A lógica se inverte para
+            o .prev(), buscando o elemento anterior.
+        */
+       
+        
     });
 });
