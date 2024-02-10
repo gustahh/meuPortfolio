7a913dca926a52c7e49a49168f0ab80878c4cb14
Gustavo Graciano <gustavocarlos206@gmail.com>
2024-02-09 21:10:42 -0300

    commit database

---
+++ js/db.js
@@ -0,0 +1,12 @@
+function Projeto(nome, descricao, mes, ano) {
+    this.nome = nome;
+    this.descricao = descricao;
+    this.url = url;
+    this.mes = mes;
+    this.dia = dia;
+    this.ano = ano;
+    this.img = img;
+}
+var chatphp = new Projeto('Chat PHP com MySQL', 'Chat em PHP com MySQL', 
+'https://github.com/gustahh/chatphp', 23, 'Outubro', 2023, '../img/chatphp.png');
+
